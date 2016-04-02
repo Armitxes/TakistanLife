78535a854dd19939ce0957504135581b19c10743
Armitxes <4rmitxes@gmail.com>
2016-04-02 17:11:45 +0200

    Takistan Life: Rise of South! Complete overhaul

---
+++ sounds.hpp
@@ -0,0 +1,41 @@
+class Siren_Short
+{
+	name		= "";
+	sound[]		= {"\Siren_Short.ogg", 1, 1};
+	titles[]	= {};
+};
+
+class Siren_Cop
+{
+	name		= "";
+	sound[]		= {"\Siren_Cop.ogg", 1, 1};
+	titles[]	= {};
+};
+
+class Siren_EMS
+{
+	name		= "";
+	sound[]		= {"\armitxes\sounds\Siren_EMS.ogg", 1, 1};
+	titles[]	= {};
+};
+
+class Bank_alarm
+{
+	name		= "";
+	sound[]		= {"\bankalarm.ogg", 1, 1};
+	titles[]	= {};
+};
+
+class Bank_alarm2
+{
+	name		= "";
+	sound[]		= {"\bankalarm2.ogg", 1, 1};
+	titles[]	= {};
+};
+
+class Punch
+{
+	name		= "";
+	sound[]		= {"\punch.ogg", 1, 1};
+	titles[]	= {};
+};
